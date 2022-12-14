64a63cb5c4d0fb49bfdf89a709a43f9b6cc37ec4
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2022-12-14 09:40:48 +0530

    Update P22.py

--- Unit 1/P22.py
+++ Unit 1/P22.py
@@ -1,4 +1,3 @@
-print("Conversion between Fahrenheit and Celsius:")
 # a) Write a Python program to convert temperatures to and from celsius, fahrenheit. [ Formula : c/5 = f-32/9 [ where c = temperature in celsius and f = temperature in fahrenheit ] 
 # Expected Output : 60°C is 140 in Fahrenheit.45°F is 7 in Celsius
 

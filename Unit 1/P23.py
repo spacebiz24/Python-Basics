477529ff7d95b079a96eb370dedc7f3ac5c33120
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2022-12-14 10:17:11 +0530

    Update P23.py

--- Unit 1/P23.py
+++ Unit 1/P23.py
@@ -1,8 +1,11 @@
 # Write a Python program to count the number of even and odd numbers from a series of numbers.
 
 list = [1,4,56,23,132,67,92,345]
+ecount = ocount = 0
 for i in list:
     if i % 2 == 0:
-        print(i, " is even")
+        ecount += 1
     else:
-        print(i, " is odd")
+        ocount += 1
+print("Even Count: ", ecount)
+print("Odd Count: ", ocount)

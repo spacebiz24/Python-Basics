800f3294b58451758b4052243fc8073c9ecd6634
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2023-01-13 00:26:05 +0530

    Corrected Unit 4/P11.py

--- Unit 4/P11.py
+++ Unit 4/P11.py
@@ -1,14 +1,18 @@
 # Write a Python class to get all possible unique subsets from a set of distinct integers.
 
-def get_subsets(array):
-    subsets = [[]]
-    for x in array:
-        new_subsets = []
-        for subset in subsets:
-            new_subsets.append(subset + [x])
-        subsets += new_subsets
-    return subsets
+class Subsets:
+    def __init__(self, array):
+        self.array = array
+    def get_subsets(self):
+        subsets = [[]]
+        for x in self.array:
+            new_subsets = []
+            for subset in subsets:
+                new_subsets.append(subset + [x])
+            subsets += new_subsets
+        return subsets
 
 s = [1, 2, 3]
-subsets = get_subsets(s)
+S1 = Subsets(s)
+subsets = S1.get_subsets()
 print(subsets)
